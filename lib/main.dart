babca4762f231fbf0793bd74d6ff59787e258f9a
farhad <farhadmohammadi5058@gmail.com>
2023-06-15 01:10:34 -0700

    working coin class

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,14 @@
 import 'package:flutter/material.dart';
 import 'price_screen.dart';
-
 void main() => runApp(MyApp());
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark().copyWith(
-          appBarTheme: const AppBarTheme(backgroundColor: Colors.lightBlue),
-          primaryColor: Colors.lightBlue,
-          scaffoldBackgroundColor: Colors.white),
-      home: PriceScreen(),
+   primaryColor: Colors.lightBlue,
+   scaffoldBackgroundColor: Colors.white),
+   home: PriceScreen(),
     );
   }
 }
